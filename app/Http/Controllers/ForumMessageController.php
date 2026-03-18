9c70a07787b06fea5ee42e9430f0dc18b5c22741
Pablo Benavides <pbenav@gmail.com>
2026-03-19 00:49:58 +0100

    Fix: Add missing Request import in ForumMessageController

--- app/Http/Controllers/ForumMessageController.php
+++ app/Http/Controllers/ForumMessageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ForumMessage;
 use App\Models\ForumThread;
 use App\Models\Team;
+use Illuminate\Http\Request;
 
 class ForumMessageController extends Controller
 {
