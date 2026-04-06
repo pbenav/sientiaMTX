b0f4d55bee621292ba159c3d22e55e1a362a341f
Pablo Benavides <pbenav@gmail.com>
2026-04-06 09:37:35 +0200

    Build: Subida de versión a la 0.8.4

--- config/app.php
+++ config/app.php
@@ -14,8 +14,8 @@ return [
     */
 
     'name' => env('APP_NAME', 'sientiaMTX'),
-    'version' => env('APP_VER', '0.8.0'),
-    'app_version' => env('APP_VER', '0.8.0'),
+    'version' => env('APP_VER', '0.8.4'),
+    'app_version' => env('APP_VER', '0.8.4'),
 
     /*
     |--------------------------------------------------------------------------
