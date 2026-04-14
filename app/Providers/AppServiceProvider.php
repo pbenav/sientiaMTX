d9926466c4e06802ec485b5c59af72494aaab767
pbenav <pbenav@gmail.com>
2026-04-14 12:23:49 +0200

    fix: force HTTPS in AppServiceProvider for correct asset generation behind proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production', 'staging') || request()->header('X-Forwarded-Proto') === 'https') {
+        if ($this->app->environment('production', 'staging', 'local') && (str_starts_with(config('app.url') ?? '', 'https') || request()->header('X-Forwarded-Proto') === 'https')) {
             URL::forceScheme('https');
         }
 
