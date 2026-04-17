ac5be79fb49f185e0d01ea3b1990f44a14cd97f2
Pablo Benavides <pbenav@gmail.com>
2026-04-17 20:50:10 +0200

    fix: import Redirect facade in GoogleController

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -7,6 +7,7 @@ use App\Services\GoogleService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redirect;
 
 use App\Traits\AwardsGamification;
 
