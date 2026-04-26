0a1c5675e430a85cf273c24fb9e8a1a6940e991f
pbenav <pbenav@gmail.com>
2026-04-26 03:21:57 +0200

    Fix Kanban view: restore @push directive and finalize div balance

--- resources/views/tasks/kanban.blade.php
+++ resources/views/tasks/kanban.blade.php
@@ -396,6 +396,7 @@
     </div>
     </div>
 
+    @push('scripts')
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
