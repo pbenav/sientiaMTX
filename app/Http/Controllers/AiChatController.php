107899e4375c0ab3e837a8aadc0b8fe9e8cddab1
Pablo Benavides <pbenav@gmail.com>
2026-05-07 23:33:57 +0200

    feat: inyectar identificadores SPDX AGPLv3 en el codigo fuente y generar SBOM de auditoria de dependencias

--- app/Http/Controllers/AiChatController.php
+++ app/Http/Controllers/AiChatController.php
@@ -1,5 +1,9 @@
 <?php
 
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (c) 2022-2026 pbenav <info@sientia.com>
+
+
 namespace App\Http\Controllers;
 
 use App\Contracts\AiAssistantInterface;
--- app/Models/TimeLog.php
+++ app/Models/TimeLog.php
@@ -1,5 +1,9 @@
 <?php
 
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (c) 2022-2026 pbenav <info@sientia.com>
+
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
