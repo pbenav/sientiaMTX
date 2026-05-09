22460b1f89480ea600efdfee7484e29ee1a6c743
pbenav <pbenav@gmail.com>
2026-05-09 19:43:00 +0200

    fix: resolve duplicate teams.tasks.update and teams.tasks.destroy route names to allow route caching

--- routes/web.php
+++ routes/web.php
@@ -124,7 +124,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/teams/{team}/tasks/bulk-update', [TaskController::class, 'bulkUpdate'])->name('teams.tasks.bulk-update');
     Route::delete('/teams/{team}/tasks/bulk-delete', [TaskController::class, 'bulkDelete'])->name('teams.tasks.bulk-delete');
     Route::post('/teams/{team}/tasks/purge-trash', [TaskController::class, 'purgeTrash'])->name('teams.tasks.purge-trash');
-    Route::resource('teams.tasks', TaskController::class)->except(['show', 'edit']);
+    Route::resource('teams.tasks', TaskController::class)->except(['show', 'edit', 'update', 'destroy']);
 
     // Override show/edit/update/destroy to avoid Scoped Binding and handle SoftDeletes
     Route::prefix('teams/{team}')->group(function() {
