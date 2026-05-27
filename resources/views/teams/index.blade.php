47aa8959df419317b27016bd034461cadfb75ef3
Pablo Benavides <pbenav@gmail.com>
2026-05-27 16:53:42 +0200

    feat(ui): add surveys quick action button to teams index cards

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -120,6 +120,15 @@
                             </svg>
                         </a>
 
+                        <!-- Encuestas -->
+                        <a href="{{ route('teams.surveys.index', $team) }}" @click.stop
+                            class="flex-1 flex items-center justify-center py-2 bg-gray-50 dark:bg-gray-800 text-gray-700 dark:text-gray-300 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-700 transition-all border border-gray-200 dark:border-gray-700"
+                            title="Encuestas">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                            </svg>
+                        </a>
+
                         <!-- Expedientes -->
                         <a href="{{ route('teams.expedientes.index', $team) }}" @click.stop
                             class="flex-1 flex items-center justify-center py-2 bg-gray-50 dark:bg-gray-800 text-gray-700 dark:text-gray-300 rounded-xl hover:bg-gray-100 dark:hover:bg-gray-700 transition-all border border-gray-200 dark:border-gray-700"
