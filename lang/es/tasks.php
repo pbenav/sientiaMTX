06551245870ad678ed95df650e563c97bfef3552
Pablo Benavides <pbenav@gmail.com>
2026-07-02 08:39:31 +0200

    chore: add missing translation for tasks.member

--- lang/es/tasks.php
+++ lang/es/tasks.php
@@ -151,6 +151,7 @@ return [
     'groups' => 'Grupos',
     'assign_groups' => 'Asignar a grupos',
     'members' => 'Miembros',
+    'member' => 'Miembro',
     'view_members' => 'Ver miembros',
     'eisenhower_matrix' => 'Matriz de Eisenhower',
     'view_gantt' => 'Ver Gantt',
