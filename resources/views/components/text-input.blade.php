b0612d86169a34ab2d5a43b7ec6b13aa2e608711
Pablo Benavides <pbenav@gmail.com>
2026-07-04 09:14:13 +0200

    fix(ui): add static type attribute to text-input to prevent password visibility flash before Alpine initialization

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -17,6 +17,7 @@
     }
 }">
     <input x-ref="input" @disabled($disabled)
+        type="{{ $type }}"
         :type="inputType"
         {{ $attributes->merge(['class' => 'pr-10 bg-gray-50 dark:bg-gray-800 border-gray-200 dark:border-gray-700 focus:border-violet-500 focus:ring-violet-500 rounded-xl shadow-sm text-gray-900 dark:text-white placeholder-gray-400 dark:placeholder-gray-500 transition-all w-full']) }}>
     
