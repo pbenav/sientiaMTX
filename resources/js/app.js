cb230308aa821bf2ce0540abb3208f2197bb763f
Pablo Benavides <pbenav@gmail.com>
2026-07-04 09:26:12 +0200

    fix(core): restore inadvertently deleted app.js content to fix AlpineJS initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,15 @@
+import './bootstrap';
 
+import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
 
+import { Passkeys } from '@laravel/passkeys';
+
+window.Alpine = Alpine;
+Alpine.plugin(collapse);
+window.Passkeys = Passkeys;
+
+// Dispatch the alpine:init event so that Blade-defined components can register themselves
+document.dispatchEvent(new CustomEvent('alpine:init'));
+
+Alpine.start();
