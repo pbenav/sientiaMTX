7973e7359875687458033de040620c4c62f4ec01
Pablo Benavides <pbenav@gmail.com>
2026-07-16 19:44:03 +0200

    Bump version to 1.1.6

--- config/app.php
+++ config/app.php
@@ -14,8 +14,8 @@ return [
     */
 
     'name' => env('APP_NAME', 'sientiaMTX'),
-    'version' => env('APP_VER', '1.1.5'),
-    'app_version' => env('APP_VER', '1.1.5'),
+    'version' => env('APP_VER', '1.1.6'),
+    'app_version' => env('APP_VER', '1.1.6'),
 
     /*
     |--------------------------------------------------------------------------
