e4fa24d8cd27ec27bc8456a49193edcba51f26ee
Pablo Benavides <pbenav@gmail.com>
2026-07-17 12:52:40 +0200

    chore(ui): rename 'Librería' to 'Biblioteca' for accurate spanish translation

--- resources/views/teams/library/index.blade.php
+++ resources/views/teams/library/index.blade.php
@@ -225,7 +225,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                         </svg>
                     </div>
-                    <h2 class="text-xl font-black text-gray-900 dark:text-white mb-2">Librería Vacía</h2>
+                    <h2 class="text-xl font-black text-gray-900 dark:text-white mb-2">Biblioteca Vacía</h2>
                     <p class="text-sm text-gray-500 max-w-md">No tienes documentos registrados en este equipo. Crea actividades de tipo "Documento" y aparecerán organizadas aquí a modo de Wiki interna.</p>
                 </div>
             @endif
