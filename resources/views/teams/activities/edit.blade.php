79d52ba0b7e0d83dafa8669e1265d7f25160be54
Pablo Benavides <pbenav@gmail.com>
2026-07-21 17:27:43 +0200

    Hide specific info card if activity type does not have dynamic fields

--- resources/views/teams/activities/edit.blade.php
+++ resources/views/teams/activities/edit.blade.php
@@ -120,6 +120,7 @@
         
                     <div x-show="activeTab === 'general'" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 translate-y-2" x-transition:enter-end="opacity-100 translate-y-0" style="display: none;">
                         <!-- BLOCK: Información Específica -->
+    @if (in_array($activity->type, ['document', 'link', 'agreement', 'meeting']))
     <div  class="bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-3xl p-6 shadow-sm transition-all hover:shadow-md mb-8 group relative overflow-hidden order-4">
         <div class="absolute top-0 right-0 p-8 opacity-5">
             <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" /></svg>
@@ -295,6 +296,7 @@
                     @endif
         </div>
     </div>
+    @endif
 
     
                     </div>
