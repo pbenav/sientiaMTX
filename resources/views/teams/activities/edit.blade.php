01391c50690aa3831d51354836b2c99ea62e1bdf
Pablo Benavides <pbenav@gmail.com>
2026-09-07 11:58:54 +0200

    fix: remove stray closing div restoring Alpine x-data scope for all tabs in edit.blade.php

--- resources/views/teams/activities/edit.blade.php
+++ resources/views/teams/activities/edit.blade.php
@@ -380,7 +380,6 @@
                 </div>
         </div>
     </div>
-    </div>
         
                     <div x-show="activeTab === 'general'" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 translate-y-2" x-transition:enter-end="opacity-100 translate-y-0" style="display: none;">
                         <!-- BLOCK: Observaciones -->
